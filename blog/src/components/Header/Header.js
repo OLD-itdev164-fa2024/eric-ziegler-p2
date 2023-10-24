cbf33efadf74d67635f2147e3dbf73c4375c3dce
unknown <zieglee1@gmatc.matc.edu>
2023-10-24 18:44:06 -0500

    created and implemented heading components

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import PropTypes from "prop-types"
+import { H1 } from "../Heading"
 
 const Outer = styled.header`
   background: ${props => props.theme.header.backgroundColor};
@@ -12,10 +13,6 @@ const Outer = styled.header`
   align-items: center;
   justify-content: space-between;
   `
-
-const H1 = styled.h1`
-  margin: 0px;
-`
 
 const StyledLink = styled(Link)`
   color: #fff;
