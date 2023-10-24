8c9fec87e7cfde265d1b26e6e4b586acd6f951e0
unknown <zieglee1@gmatc.matc.edu>
2023-10-24 18:56:19 -0500

    implemented semantic components

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -12,6 +12,8 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import { Gray } from "./themes/Gray"
 import { Header } from "./Header"
+import { Main } from "./Main"
+import { Footer } from "./Footer"
 import "./layout.css"
 
 const Content = styled.div`
@@ -35,8 +37,8 @@ const Layout = ({ children }) => {
     <ThemeProvider theme={Gray}>
       <Header siteTitle={data.site.siteMetadata.title || `Title`} />
       <Content>
-        <main>{children}</main>
-        <footer
+        <Main>{children}</Main>
+        <Footer
           style={{
             marginTop: `var(--space-5)`,
             fontSize: `var(--font-sm)`,
@@ -45,7 +47,7 @@ const Layout = ({ children }) => {
           © {new Date().getFullYear()} &middot; Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        </Footer>
       </Content>
     </ThemeProvider>
   )
