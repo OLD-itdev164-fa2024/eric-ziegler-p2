ce1b3b3e95b8e2eca749d81ce11f4b44f861d953
unknown <zieglee1@gmatc.matc.edu>
2023-11-24 17:28:05 -0600

    game page now has functioning travel links

--- blog/src/templates/game-page.js
+++ blog/src/templates/game-page.js
@@ -1,17 +1,30 @@
 import React from "react";
+import { Link } from "gatsby"
 import { graphql } from "gatsby";
 import Layout from '../components/layout';
 import { GatsbyImage } from "gatsby-plugin-image";
 import { H1 } from "../components/Heading";
 
 const GamePage = ({data}) => {
-    const { title, mapImage, areaDescription } = data.contentfulGameTile;
+    const { title, mapImage, areaDescription, north, east, south, west } = data.contentfulGameTile;
 
     return (
         <Layout>
             <div><GatsbyImage image={mapImage.gatsbyImageData} /></div>
             <H1>{title}</H1>
-            <div dangerouslySetInnerHTML={{__html: areaDescription.childMarkdownRemark.html}}></div>
+        <div dangerouslySetInnerHTML={{ __html: areaDescription.childMarkdownRemark.html }}></div>
+        {north == null ? null :
+          <div><Link to={`/${north.slug}`}>Go North</Link>: {north.connectionHint}</div>
+        }
+        {east == null ? null :
+          <div><Link to={`/${east.slug}`}>Go East</Link>: {east.connectionHint}</div>
+        }
+        {south == null ? null :
+          <div><Link to={`/${south.slug}`}>Go South</Link>: {south.connectionHint}</div>
+        }
+        {west == null ? null :
+          <div><Link to={`/${west.slug}`}>Go West</Link>: {west.connectionHint}</div>
+        }
         </Layout>
     )
 }
