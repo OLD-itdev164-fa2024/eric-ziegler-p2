ec88ebaf8d2b90ebed4439a43dc9f908ba2e0f83
unknown <zieglee1@gmatc.matc.edu>
2023-11-26 17:23:27 -0600

    fixed config and icon issue from cleanup

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/img`,
       },
     },
     {
@@ -54,7 +54,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/img/ez.png`, // This path is relative to the root of the site.
       },
     },
   ],
