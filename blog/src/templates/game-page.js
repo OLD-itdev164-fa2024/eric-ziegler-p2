29d3ad8bd8a0e2aefb664dd5b2c396f9c66e40bf
unknown <zieglee1@gmatc.matc.edu>
2023-12-03 18:08:56 -0600

    added layout components and styling

--- blog/src/templates/game-page.js
+++ blog/src/templates/game-page.js
@@ -3,11 +3,26 @@ import { Link } from "gatsby"
 import { graphql } from "gatsby";
 import Layout from '../components/layout';
 import { GatsbyImage } from "gatsby-plugin-image";
+import styled from 'styled-components'
+import { Box } from "rebass";
 import { H1 } from "../components/Heading";
 import { TravelLink } from "../components/TravelLink";
 import { GameEncounter } from "../components/GameEncounter";
 import { Heart } from "styled-icons/entypo";
 import { Skull, RotateLeft } from "styled-icons/fa-solid";
+
+const GameContent = styled(Box)`
+  box-sizing: border-box;
+  margin: 0 auto;
+  min-width: 0px;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+`
+
+const MapTitle = styled(H1)`
+  text-align:center;
+`
 
 var hp = 0
 var showEncounter = false
@@ -43,41 +58,41 @@ const GamePage = ({data}) => {
 
   return (
     <Layout>
-      <div><GatsbyImage image={mapImage.gatsbyImageData} /></div>
-      <H1>{title}</H1>
-      
-      {showEncounter
-        ? <GameEncounter encounter={encounter} />
-        : null}
+      <GameContent>
+        <div><GatsbyImage image={mapImage.gatsbyImageData} /></div>
+        <MapTitle>{title}</MapTitle>
+        <div dangerouslySetInnerHTML={{ __html: areaDescription.childMarkdownRemark.html }}></div>
         
-      <div dangerouslySetInnerHTML={{ __html: areaDescription.childMarkdownRemark.html }}></div>
-      
-      {hp > 0
-        ? 
-        <React.Fragment>
-          <p><Heart size={24} color="red"/> {hp}</p>
-          {north == null ? null :
-            <TravelLink direction="n" tile={north} />
-          }
-          {east == null ? null :
-            <TravelLink direction="e" tile={east} />
-          }
-          {south == null ? null :
-            <TravelLink direction="s" tile={south} />
-          }
-          {west == null ? null :
-            <TravelLink direction="w" tile={west} />
-          }
-        </React.Fragment>
-
-        :
-        <React.Fragment>
-          <p><Skull size={24} color="red"/> {hp}</p>
-          <div>You have fallen. Your adventure is at an end. Better luck next time!</div>
-          <Link to="/"><RotateLeft size={24}/>Return to the main page.</Link>
-        </React.Fragment>
-      }
-    
+        {showEncounter
+          ? <GameEncounter encounter={encounter} />
+          : null}
+        
+        
+        {hp > 0
+          ?
+          <React.Fragment>
+            <p><Heart size={24} color="red"/> {hp}</p>
+            {north == null ? null :
+              <TravelLink direction="n" tile={north} />
+            }
+            {east == null ? null :
+              <TravelLink direction="e" tile={east} />
+            }
+            {south == null ? null :
+              <TravelLink direction="s" tile={south} />
+            }
+            {west == null ? null :
+              <TravelLink direction="w" tile={west} />
+            }
+          </React.Fragment>
+          :
+          <React.Fragment>
+            <p><Skull size={24} color="red"/> {hp}</p>
+            <div>You have fallen. Your adventure is at an end. Better luck next time!</div>
+            <Link to="/"><RotateLeft size={24}/>Return to the main page.</Link>
+          </React.Fragment>
+        }
+      </GameContent>
     </Layout>
   )
 }
